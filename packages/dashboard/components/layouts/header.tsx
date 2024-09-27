043df17fea57297f1368f4db396d9ca7dc6b12fa
Nasim Akhtar <fnlog10@gmail.com>
2024-09-27 12:13:32 +0530

    updated nav

--- packages/dashboard/components/layouts/header.tsx
+++ packages/dashboard/components/layouts/header.tsx
@@ -1,7 +1,29 @@
+import Link from "next/link";
+
 import { ModeToggle } from "./mode-toggle";
 import ProfileMenu from "../profile-menu";
+import { Icons } from "@/components/icons";
 
 function Header() {
+  return (
+    <header className="border-b py-2 px-5 sticky top-0 z-50 w-full bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+      <div className="container flex h-14 max-w-screen-2xl items-center">
+        <div className="mr-4 flex">
+          <Link href="/" className="mr-4 flex items-center space-x-1 lg:mr-6">
+            <Icons.logo className="h-6 w-6" />
+            <span className="hidden font-bold lg:inline-block">
+              Effortless Labs /
+            </span>
+            <span className="hidden lg:inline-block">Logpipe</span>
+          </Link>
+        </div>
+        <div className="flex flex-1 items-center justify-between space-x-2 md:justify-end">
+          <ModeToggle />
+          <ProfileMenu />
+        </div>
+      </div>
+    </header>
+  );
   return (
     <div className="border-b py-3 px-5 flex justify-between sticky top-0 z-50 bg-background">
       <h1 className="font-bold text-2xl">
