5a359f1046ce6f24195493c7c8fb308057eb53e5
Suyash Srivastava <suyashsrivastava5053@gmail.com>
2024-09-27 11:49:00 +0530

    feat: logout and name change

--- packages/dashboard/components/profile-menu/general/index.tsx
+++ packages/dashboard/components/profile-menu/general/index.tsx
@@ -1,5 +1,11 @@
 "use client";
 
+import { useCallback, useState, useEffect } from "react";
+import { useQuery, useMutation } from "@apollo/client";
+import { GET_PROFILE, UPDATE_PROFILE } from "@/graphql/profile";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -7,18 +13,45 @@ import {
   CardFooter,
   CardTitle,
 } from "@/components/ui/card";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { Separator } from "@/components/ui/separator";
-import { Button } from "@/components/ui/button";
-import { GET_PROFILE } from "@/graphql/profile";
-import { useQuery } from "@apollo/client";
-import { Badge } from "@/components/ui/badge";
 import CopyButton from "@/components/copy-button";
 
 function General() {
   const { data } = useQuery(GET_PROFILE);
-  const profile_email = data?.profile.email;
-  const profile_id = data?.profile.id;
+  const [updateProfile] = useMutation(UPDATE_PROFILE);
+  const [displayName, setDisplayName] = useState("");
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [error, setError] = useState("");
+  const [isUpdating, setIsUpdating] = useState(false);
+  useEffect(() => {
+    if (isUpdating) return;
+    const trimmedName = displayName.trim();
+    if (trimmedName === data?.profile.name) {
+      setError("The new name can't be the same as the old name.");
+      setIsDisabled(true);
+    } else if (trimmedName.length < 3 && trimmedName.length > 0) {
+      setError("The name should be at least 3 characters long.");
+      setIsDisabled(true);
+    } else {
+      setError("");
+      setIsDisabled(false);
+    }
+  }, [displayName, data, isUpdating]);
+  const handleSubmit = useCallback(
+    async (displayName: string) => {
+      try {
+        setIsUpdating(true);
+        await updateProfile({
+          variables: { input: { name: displayName } },
+        });
+        await window.location.reload();
+      } catch (error) {
+        console.error("Error updating profile:", error);
+      }
+    },
+    [updateProfile]
+  );
+
   return (
     <div className="flex flex-col gap-5">
       <Card className="flex flex-col p-5 gap-5">
@@ -52,13 +85,20 @@ function General() {
           <input
             type="text"
             className="w-56 h-8 border border-foreground-muted rounded-md p-2 outline-foreground/50"
-            placeholder=""
+            placeholder={data?.profile.name}
+            value={displayName}
+            onChange={(e) => setDisplayName(e.target.value)}
           />
         </CardContent>
+        {error && <p className="text-red-500 text-sm">{error}</p>}
         <Separator />
         <CardFooter className="flex justify-between p-0 text-sm text-foreground">
           Please use 32 characters at maximum.
-          <Button variant={"ghost"} className="text-foreground">
+          <Button
+            variant={"default"}
+            onClick={() => handleSubmit(displayName)}
+            disabled={isDisabled}
+          >
             Save
           </Button>
         </CardFooter>
@@ -70,7 +110,7 @@ function General() {
           primary email will be used for account-related notifications.
         </CardDescription>
         <div className="rounded-md h-12 w-96 p-2 border border-foreground-muted flex items-center justify-between">
-          {profile_email} <Badge>Default</Badge>
+          {data?.profile.email} <Badge>Default</Badge>
         </div>
         <Separator />
         <CardFooter className="p-0 text-foreground text-sm">
@@ -83,7 +123,7 @@ function General() {
           This is your id with LogPipe.
         </CardDescription>
         <div className="h-10 w-72 border border-foreground-muted rounded-md flex items-center p-2 justify-between">
-          {profile_id} <CopyButton element={data?.profile.id} />
+          {data?.profile.id} <CopyButton element={data?.profile.id} />
         </div>
         <Separator />
         <CardFooter className="p-0 text-foreground text-sm">
--- packages/dashboard/zustand/profile.ts
+++ packages/dashboard/zustand/profile.ts
@@ -4,9 +4,11 @@ import { create } from "zustand";
 interface ProfileState {
   profile: Profile | null;
   setProfile: (profile: Profile) => void;
+  clearProfile: () => void;
 }
 
 export const useProfileStore = create<ProfileState>((set) => ({
   profile: null,
   setProfile: (profile) => set({ profile }),
+  clearProfile: () => set({ profile: null }),
 }));
